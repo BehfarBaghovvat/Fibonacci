7e5fe0f22f30728b360f04c50e346fbced85756c
Behfar Baghovvat <46481959+BehfarBaghovvat@users.noreply.github.com>
2019-01-10 03:16:22 +0330

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int FirstNumber, SecondNumber, Result, EnterNumber;
+
+            FirstNumber = 1;
+            SecondNumber = 1;
+
+            System.Console.WriteLine
+                ("          ==========================SHOW FIBONACCI==========================");
+
+            System.Console.WriteLine();
+
+            System.Console.Write("Please Enter Number: ");
+
+            EnterNumber = System.Convert.ToInt32
+                (System.Console.ReadLine());
+
+            System.Console.WriteLine();
+            
+            System.Console.Write
+                (FirstNumber+", " + SecondNumber+", ");
+
+            for (int i = 0; i <= EnterNumber; i++)
+            {
+                Result = FirstNumber + SecondNumber;
+                
+                System.Console.Write(Result+", ");
+
+                FirstNumber = SecondNumber;
+
+                SecondNumber = Result;
+
+            }
+
+            System.Console.WriteLine();
+            System.Console.WriteLine();
+            System.Console.WriteLine();
+            
+            System.Console.Write("Please [ENTER] To EXIT... ");
+            System.Console.Read();
+
+        }
+    }
+}
